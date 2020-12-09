441f3161f87195ab6154e65ae48f18fb7403ea9e
sarahertog <sarahertog@gmail.com>
2020-12-09 10:44:08 -0500

    fix unknown redist

--- dd_distribute_unknowns.R
+++ dd_distribute_unknowns.R
@@ -16,15 +16,16 @@ dd_distribute_unknowns <- function(data){
     
     total_value   <- df_abr$DataValue[df_abr$AgeLabel == "Total"]
     unknown_value <- df_abr$DataValue[df_abr$AgeLabel == "Unknown"]
+    total_less_unknown <- total_value - unknown_value
     
     if (!is_empty(total_value)) {
-    df_abr <- df_abr %>% 
-      mutate(pct_dist = DataValue/total_value,
-             add_unk  = unknown_value * pct_dist,
-             add_unk  = ifelse(AgeLabel == "Total", 0, add_unk),
-             DataValue = DataValue + add_unk) %>% 
-      filter(AgeLabel != "Unknown") %>% 
-      select(-pct_dist, -add_unk)
+      df_abr <- df_abr %>% 
+        mutate(pct_dist = DataValue/total_less_unknown,
+               add_unk  = unknown_value * pct_dist,
+               add_unk  = ifelse(AgeLabel == "Total", 0, add_unk),
+               DataValue = DataValue + add_unk) %>% 
+        filter(AgeLabel != "Unknown") %>% 
+        select(-pct_dist, -add_unk)
     }
     rm(total_value, unknown_value)
     
@@ -33,10 +34,11 @@ dd_distribute_unknowns <- function(data){
     
     total_value   <- df_cpl$DataValue[df_cpl$AgeLabel == "Total"]
     unknown_value <- df_cpl$DataValue[df_cpl$AgeLabel == "Unknown"]
+    total_less_unknown <- total_value - unknown_value
     
     if (!is_empty(total_value)) {
       df_cpl <- df_cpl %>% 
-        mutate(pct_dist = DataValue/total_value,
+        mutate(pct_dist = DataValue/total_less_unknown,
                add_unk  = unknown_value * pct_dist,
                add_unk  = ifelse(AgeLabel == "Total", 0, add_unk),
                DataValue = DataValue + add_unk) %>% 
@@ -44,9 +46,10 @@ dd_distribute_unknowns <- function(data){
         select(-pct_dist, -add_unk)
     }
     
-  out.data <- rbind(out.data, df_abr, df_cpl)
+    out.data <- rbind(out.data, df_abr, df_cpl)
   }
   
   return(out.data)
   
 }
+
