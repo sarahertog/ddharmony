892aee91f4ad35872b2e5239d1d9eb82b2fa9373
sarahertog <sarahertog@gmail.com>
2021-03-03 10:02:05 -0500

    Discard a Turkmenistan 1979 dup

--- discard_these_dups.R
+++ discard_these_dups.R
@@ -4,6 +4,7 @@
 
 discard_these_dups <- c("535 - Bonaire, Sint Eustatius and Saba - Census - 1992 - Demography of The Netherlands Antilles - An Analyses of Demographic Variables - Unknown - Population by age and sex - Fair",
                         "398 - Kazakhstan - Census - 1959 - All-Union Census of 1959. Table 2.5. Distribution of the total population and married by sex and age. Kazakhstan - De-jure - Population by age and sex - High quality",
+                        "795 - Turkmenistan - Census - 1979 - All-Union Census 1970 Table 5c. Distribution of the entire population and married by sex and age. RGAE RF F. 1562, op. 336, ed.hr. 3998-4013. - De-facto - Population by age and sex - Fair",
                         "44 - Bahamas - VR - Births - 2003 - Register - CaribbeanStatsCHAP1-POPFAM - Year of occurrence - Direct - Fair",
                         "44 - Bahamas - VR - Births - 2002 - Register - CaribbeanStatsCHAP1-POPFAM - Year of occurrence - Direct - Fair",
                         "44 - Bahamas - VR - Births - 2001 - Register - CaribbeanStatsCHAP1-POPFAM - Year of occurrence - Direct - Fair",
