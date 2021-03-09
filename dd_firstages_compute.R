840f66fb3747809f8882c628c5bf580bd28d881c
sarahertog <sarahertog@gmail.com>
2021-03-09 11:30:32 -0500

    Add reconciliation of missing age 0-1 to first_ages_compute

--- dd_firstages_compute.R
+++ dd_firstages_compute.R
@@ -10,17 +10,23 @@ dd_firstages_compute <- function(data){
   
   df <- dd_age_standard(data, abridged = TRUE)
   
-  # if 0-4 is missing and 0-1 and 1-4 are present, then sum to 0-4
   dv01 <- df$DataValue[df$AgeLabel == "0"]
   dv14 <- df$DataValue[df$AgeLabel == "1-4"]
   dv04 <- df$DataValue[df$AgeLabel == "0-4"]
 
+  # if 0-4 is missing and 0-1 and 1-4 are present, then sum to 0-4
   if(!is.na(dv14) & !is.na(dv01) & is.na(dv04)) {
     df$DataValue[df$AgeLabel == "0-4"] <- dv01 + dv14
   }
   
+  # if 1-4 is missing and 0-1 and 0-4 are present, then compute 1-4
   if(is.na(dv14) & !is.na(dv01) & !is.na(dv04)) {
     df$DataValue[df$AgeLabel == "1-4"] <- dv04 - dv01
+  }
+  
+  # if 0-1 is missing and 0-4 and 1-4 are present, then compute 0-1
+  if(is.na(dv01) & !is.na(dv04) & !is.na(dv14)) {
+    df$DataValue[df$AgeLabel == "0"] <- dv04 - dv14
   }
   
   data.out <- df %>% 
