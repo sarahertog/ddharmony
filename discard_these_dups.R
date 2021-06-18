ceb400bb9b07eb239e825736bfb4de1fdbcf5878
Sara Hertog <Sara.Hertog@unhq.un.org>
2021-06-17 20:47:13 -0400

    some documentation

--- discard_these_dups.R
+++ discard_these_dups.R
@@ -1,6 +1,7 @@
 
 ## temporary file to identify specific duplicate sources in DemoData for deletion
 ## we are not able to filter these out through the existing ranking criteria
+## soon obsolete -- should not need this
 
 discard_these_dups <- c("535 - Bonaire, Sint Eustatius and Saba - Census - 1992 - Demography of The Netherlands Antilles - An Analyses of Demographic Variables - Unknown - Population by age and sex - Fair",
                         "398 - Kazakhstan - Census - 1959 - All-Union Census of 1959. Table 2.5. Distribution of the total population and married by sex and age. Kazakhstan - De-jure - Population by age and sex - High quality",
